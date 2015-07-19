0e5c3794f16e1d564b4479dd9392fa45d635d7fe
Chris Tozer <christopherjtozer@gmail.com>
2015-07-19 15:51:51 +0100

    Install ack by default

--- manifests/default.pp
+++ manifests/default.pp
@@ -5,4 +5,5 @@ package { "tmux": }
 package { "vim": }
 package { "python-pip": }
 package { "gcc": }
+package { "ack-grep": }
 package { "powerline-status": provider => "pip", require => Package['python-pip'] }
